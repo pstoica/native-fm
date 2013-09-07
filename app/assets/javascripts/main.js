d6eb825711415ae4761af7413993fcfb4983e7a0
Patrick Stoica <pstoica@gatech.edu>
2013-09-07 11:41:33 -0400

    angular should be good to go now~

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('nativeFM', [
+  'restangular',
+  'ng-rails-csrf',
+  'ajoslin.promise-tracker',
+  'nativeFM.filters',
+  'nativeFM.services',
+  'nativeFM.directives',
+  'nativeFM.controllers'
+]);
