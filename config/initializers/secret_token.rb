04af1e9aea52ae78f1b18e9b1fc70b6703310151
Patrick Stoica <pstoica@gatech.edu>
2013-09-06 23:28:57 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NativeFm::Application.config.secret_key_base = '9642d4e44a7906ab1ea156a8a2f1b56280bb800ecc47c47ba5340ee6f1fc9f6d1e64e53805da2e2a223a89d2a9c0c5ec8fde01a412d86310767afaf2ae25adf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NativeFm::Application.config.session_store :cookie_store, key: '_native-fm_session'
