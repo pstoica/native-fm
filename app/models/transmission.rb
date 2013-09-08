05f03ad249e3ae9808d828cc77e1a2ea4ea00968
Kyle Petrovich <kylepetrovich@gmail.com>
2013-09-08 10:44:17 -0400

    fixed shitty validation

--- app/models/transmission.rb
+++ app/models/transmission.rb
@@ -4,7 +4,7 @@ class Transmission < ActiveRecord::Base
   belongs_to :receiver, :class_name => "User"
   belongs_to :song
 
-  validates :song, presence: true
+  validates :song_id, presence: true
 
   after_save(on: :create) do
     Delayed::Job.enqueue MatchTransmission.new(self), queue: 'matching'
